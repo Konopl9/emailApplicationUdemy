fb8027dee59a8b16e7c39fbd0ce6edcc534ee45e
Konopl9 <maksimus2998@gmail.com>
2022-01-16 18:01:54 +0100

    EA-1 first commit

---
+++ src/emaiapp/Email.java
@@ -0,0 +1,25 @@
+package emaiapp;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String departments;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor ro receive first name and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Ask for department
+
+    // Generate a random password
+
+    // Set mailbox capacity
+
+    // Change the password
+}
---
+++ src/emaiapp/EmailApp.java
@@ -0,0 +1,8 @@
+package emaiapp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+
+    }
+}
