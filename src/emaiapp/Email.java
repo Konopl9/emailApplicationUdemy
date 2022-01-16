274f2339b395f2d513aa93bb6aca9133fb403ca3
Konopl9 <maksimus2998@gmail.com>
2022-01-16 18:49:42 +0100

    EA-2 finished email application

--- src/emaiapp/Email.java
+++ src/emaiapp/Email.java
@@ -1,25 +1,87 @@
 package emaiapp;
 
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
     private String lastName;
     private String password;
-    private String departments;
-    private int mailboxCapacity;
+    private String department;
+    private String email;
+    private int mailboxCapacity = 500;
+    private int defaultPasswordLength = 10;
     private String alternateEmail;
+    private String companySuffix = "aeycompamy.com";
 
     // Constructor ro receive first name and last name
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        // Call a method asking for a department - return a department
+        this.department = setDepartment();
+        // Call a method that returns a random password
+        this.password = randomPassword(defaultPasswordLength);
+        // Combine elements to generate the email
+        this.email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
     }
 
     // Ask for department
+    private String setDepartment() {
+        System.out.println("New worker: " + firstName + ". DEPARTMENT CODES:\n1 for Sales \n2 for Development\n3 for Accounting\n0 for none\n Enter department code");
+        Scanner in = new Scanner(System.in);
+        int departmentChoice = in.nextInt();
+
+        if (departmentChoice == 1) { return "sales"; }
+        else if (departmentChoice == 2) { return "development"; }
+        else if (departmentChoice == 3) { return "accounting"; }
+        else { return ""; }
+    }
 
     // Generate a random password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+
+        // Generate password
+        for(int i = 0; i < length; i++) {
+            int random = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(random);
+        }
+
+        return new String(password);
+    }
 
     // Set mailbox capacity
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+    }
+
+    // Set alternative email
+    public void setAlternateEmail(String alternateEmail) {
+        this.alternateEmail = alternateEmail;
+    }
 
     // Change the password
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String showInfo() {
+        return "DISPLAY NAME: " + firstName + " " + lastName +
+                "\nCOMPAMY EMAIL: " + email +
+                "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+    }
 }
--- src/emaiapp/EmailApp.java
+++ src/emaiapp/EmailApp.java
@@ -3,6 +3,8 @@ package emaiapp;
 public class EmailApp {
 
     public static void main(String[] args) {
+        Email email = new Email("Max", "Mishchenko");
 
+        System.out.println(email.showInfo());
     }
 }
